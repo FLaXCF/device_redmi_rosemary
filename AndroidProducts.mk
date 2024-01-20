08828f3a3f16bd9d97a3e88f67dc24da3f158b6a
Fela <60063027+FLaXCF@users.noreply.github.com>
2024-01-20 02:18:21 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,4 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/crdroid_rosemary.mk
 
 COMMON_LUNCH_CHOICES := \
-    $(foreach variant, user userdebug eng, aosp_rosemary-$(variant))
+    $(foreach variant, user userdebug eng, crdroid_rosemary-$(variant))
